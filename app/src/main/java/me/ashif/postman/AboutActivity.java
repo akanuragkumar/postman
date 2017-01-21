cfb354325043a44b5799230da90590df017042ad
Anurag Kumar <akanuragkumar712@gmail.com>
2017-01-21 23:58:37 +0530

    Update AboutActivity.java

--- app/src/main/java/me/ashif/postman/AboutActivity.java
+++ app/src/main/java/me/ashif/postman/AboutActivity.java
@@ -1,7 +1,7 @@
 package me.ashif.postman;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.os.Bundle; 
 
 public class AboutActivity extends AppCompatActivity {
 
